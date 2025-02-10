fbb90ef1a6f05268e369111ed8f8729d19b16826
Aman Kumar <aman140426@gmail.com>
2025-02-10 16:10:45 +0530

    a

--- promises/app.js
+++ promises/app.js
@@ -83,15 +83,23 @@
 
 //consumePromiseFive();
 
-async function getAllUser(params) {
-    try {
-      const response= await fetch("https://jsonplaceholder.typicode.com/users")
-      const data= await response.json();
-      console.log(data);
-    } catch (error) {
-      console.log("E: ",error)
-    }
+// async function getAllUser(params) {
+//     try {
+//       const response= await fetch("https://jsonplaceholder.typicode.com/users")
+//       const data= await response.json();
+//       console.log(data);
+//     } catch (error) {
+//       console.log("E: ",error)
+//     }
   
-  }
+//   }
   
-  getAllUser();
+//   getAllUser();
+
+
+fetch('https://jsonplaceholder.typicode.com/users')
+.then((response)=>{
+  return response.json();
+})
+.then((data)=> console.log(data))
+.catch( (error)=>console.log(error))
